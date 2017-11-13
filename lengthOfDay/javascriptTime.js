8809c4e2c27ecfb441a1a448ad2574d1fcfff80a
LK Loh <lkloh2410@gmail.com>
2017-11-13 09:13:53 -0800

    length of day

--- lengthOfDay/javascriptTime.js
+++ lengthOfDay/javascriptTime.js
@@ -10,8 +10,34 @@ function lastWeek(current_date) {
 	return new Date(current_date - WEEK_MILLISECONDS);
 }
 
-var today = new Date()
-console.log('Yesterday: ' + yesterday(today));
-console.log('Last Week: ' + lastWeek(today));
+function whatHappensNow() {
+	console.log(`\nWhat happens now`);
+	var today = new Date();
+	console.log(`Current time: ${today}`);
+	console.log(`Yesterday: ${yesterday(today)}`);
+	console.log(`Last Week: ${lastWeek(today)}`);
+	console.log(`\n`);
+}
 
+function onDaylightsSavingTransition() {
+	console.log(`\nWhat happens on daylights saving`);
+	var today = new Date('2017-11-05T02:00:00');
+	console.log(`Current time: ${today}`);
+	console.log(`Yesterday: ${yesterday(today)}`);
+	console.log(`Last Week: ${lastWeek(today)}`);
+	console.log(`\n`);
+}
+
+function justBeforeDaylightsSavingTransition() {
+	console.log(`\nWhat happens just before daylights saving`);
+	var today = new Date('2017-11-05T01:59:00');
+	console.log(`Current time: ${today}`);
+	console.log(`Yesterday: ${yesterday(today)}`);
+	console.log(`Last Week: ${lastWeek(today)}`);
+	console.log(`\n`);
+}
+
+whatHappensNow();
+onDaylightsSavingTransition();
+justBeforeDaylightsSavingTransition();
 
