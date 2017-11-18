2c658e3ece1aa6c1cca9c5720410d478c7bcd3a8
LK Loh <lkloh2410@gmail.com>
2017-11-18 15:09:57 -0800

    Update

--- leapSecond.js
+++ leapSecond.js
@@ -9,4 +9,10 @@ console.log(Date.UTC(2017, 0, 2, 0, 0, 0) - Date.UTC(2016, 11, 31, 0, 0, 0));
 console.log('Not leap second: ' + new Date(Date.UTC(2017, 11, 31, 23, 59, 59) + 1000).toUTCString());
 console.log(Date.UTC(2018, 0, 2, 0, 0, 0) - Date.UTC(2017, 11, 31, 0, 0, 0));
 
-console.log('Geek with a hat: ' + new Date(new Date("1999-01-01T00:00:00.00").getTime()-1000*60*60*24));
+console.log('Geek with a hat, 1999: ' + new Date(new Date("1999-01-01T00:00:00.00").getTime()-1000*60*60*24));
+
+console.log();
+oct15_2017 = new Date(new Date("2017-10-15T00:00:00.00").getTime());
+console.log('Today: ' + oct15_2017);
+console.log('Yesterday: ' + new Date(oct15_2017.setDate(oct15_2017.getDate()-1)));
+console.log('24 hours ago: ' + new Date(new Date("2017-10-15T00:00:00.00").getTime()-1000*60*60*24));
