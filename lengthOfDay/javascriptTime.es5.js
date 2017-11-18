c6cd67a212d6414afba11893fc89242e19460600
LK Loh <lkloh2410@gmail.com>
2017-11-18 11:18:50 -0800

    Daylights saving gotchas

--- lengthOfDay/javascriptTime.es5.js
+++ lengthOfDay/javascriptTime.es5.js
@@ -2,12 +2,19 @@ var DAY_MILLISECONDS = 1000 * 60 * 60 * 24;
 var WEEK_MILLISECONDS = DAY_MILLISECONDS * 7;
 
 
-var daylights_saving_transition = new Date('2017-11-05T02:00:00');
-console.log('Offset on daylights saving: ' + daylights_saving_transition);
-console.log('Offset on day before daylights saving: ' + new Date(daylights_saving_transition.getTime() - DAY_MILLISECONDS));
+var regular_day = new Date('2017-11-01T02:00:00');
+console.log('On daylights saving: ' + regular_day);
+console.log('Before daylights saving: ' + new Date(regular_day.getTime() - DAY_MILLISECONDS));
 
 console.log();
 
 var after_daylights_saving_transition = new Date('2017-11-06T02:00:00');
-console.log('Offset after daylights saving: ' + after_daylights_saving_transition);
-console.log('Offset on daylights saving: ' + new Date(after_daylights_saving_transition.getTime() - DAY_MILLISECONDS));
+console.log('After daylights saving: ' + after_daylights_saving_transition);
+console.log('On daylights saving: ' + new Date(after_daylights_saving_transition.getTime() - DAY_MILLISECONDS));
+
+console.log();
+
+console.log('After daylights saving: ' + after_daylights_saving_transition);
+var day_before_daylights_saving = new Date(after_daylights_saving_transition);
+day_before_daylights_saving.setDate(day_before_daylights_saving.getDate() - 1);
+console.log('On daylights saving: ' + day_before_daylights_saving);
