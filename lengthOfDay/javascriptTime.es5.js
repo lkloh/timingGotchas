a5b5fbdc73a993ed8cb3db124b30041d8a82f124
LK Loh <lkloh2410@gmail.com>
2017-11-18 11:15:34 -0800

    Update date

--- lengthOfDay/javascriptTime.es5.js
+++ lengthOfDay/javascriptTime.es5.js
@@ -3,11 +3,11 @@ var WEEK_MILLISECONDS = DAY_MILLISECONDS * 7;
 
 
 var daylights_saving_transition = new Date('2017-11-05T02:00:00');
-console.log('Offset on daylights saving: ' + daylights_saving_transition.getTimezoneOffset());
-console.log('Offset on day before daylights saving: ' + new Date(daylights_saving_transition.getTime() - DAY_MILLISECONDS).getTimezoneOffset());
+console.log('Offset on daylights saving: ' + daylights_saving_transition);
+console.log('Offset on day before daylights saving: ' + new Date(daylights_saving_transition.getTime() - DAY_MILLISECONDS));
 
 console.log();
 
 var after_daylights_saving_transition = new Date('2017-11-06T02:00:00');
-console.log('Offset after daylights saving: ' + after_daylights_saving_transition.getTimezoneOffset());
-console.log('Offset on daylights saving: ' + new Date(after_daylights_saving_transition.getTime() - DAY_MILLISECONDS).getTimezoneOffset());
+console.log('Offset after daylights saving: ' + after_daylights_saving_transition);
+console.log('Offset on daylights saving: ' + new Date(after_daylights_saving_transition.getTime() - DAY_MILLISECONDS));
