ba462793f3911c3ed504c01c34a266433f820459
LK Loh <lkloh2410@gmail.com>
2017-11-18 14:27:56 -0800

    millisnd

--- leapSecond.js
+++ leapSecond.js
@@ -1,11 +1,10 @@
 
+console.log( new Date(Date.UTC(2016, 11, 31, 23, 59, 59)).toUTCString() );
 
 // December 31, 23:59:60 leap second
-var leapSecond = new Date(Date.UTC(2016, 12, 31, 23, 59, 60)); 
-console.log('Last leap second: ' + leapSecond);
+console.log('Last leap second: ' + new Date(Date.UTC(2016, 11, 31, 23, 59, 59) + 1000).toUTCString());
 
 // today
-var notLeapSecond = new Date(Date.UTC(2017, 12, 31, 23, 59, 60));
-console.log(notLeapSecond);
+console.log('Not leap second: ' + new Date(Date.UTC(2017, 11, 31, 23, 59, 59) + 1000).toUTCString());
 
 
