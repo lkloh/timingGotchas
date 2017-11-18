592a31dd18dbb1f3e54c081d348e6d273bd67c33
LK Loh <lkloh2410@gmail.com>
2017-11-18 11:00:00 -0800

    Updated

---
+++ lengthOfDay/javascriptTime.es5.js
@@ -0,0 +1,11 @@
+
+
+
+var daylights_saving_transition = new Date('2017-11-05T02:00:00');
+console.log(daylights_saving_transition.getTimezoneOffset());
+
+
+var after_daylights_saving_transition = new Date('2017-11-06T02:00:00');
+console.log(after_daylights_saving_transition.getTimezoneOffset());
+
+
