acac46d8ff778aeecd9bbfe0fe3e5237de8fd3de
LK Loh <lkloh2410@gmail.com>
2017-11-18 17:25:06 -0800

    leap seconds

--- leapSecond.js
+++ leapSecond.js
@@ -22,3 +22,24 @@ jan1_1999 = new Date(new Date("1999-01-01T00:00:00.00").getTime());
 console.log('Today: ' + jan1_1999);
 console.log('Yesterday: ' + new Date(jan1_1999.setDate(jan1_1999.getDate()-1)));
 console.log('24 hours ago: ' + new Date(new Date("1999-01-01T00:00:00.00").getTime()-1000*60*60*24));
+
+
+
+var moment = require('moment');
+var dec31_1998_utc = moment.utc('1998-12-31 23:59:59', 'YYYY-MM-DD HH:mm:ss');
+dec31_1998_utc.add(1, 'seconds');
+console.log(dec31_1998_utc.toString());
+
+var jan1_1999_utc = moment.utc('1999-01-01 00:00:00', 'YYYY-MM-DD HH:mm:ss');
+jan1_1999_utc.subtract(24*60*60, 'seconds');
+console.log(jan1_1999_utc.toString());
+
+var jan1_1999_yesterday_utc = moment.utc('1999-01-01 00:00:00', 'YYYY-MM-DD HH:mm:ss');
+jan1_1999_yesterday_utc.subtract(1, 'days');
+console.log(jan1_1999_utc.toString());
+
+
+
+
+
+
